2119576b8c02cac6135d3633196b22268f938ac4
gokimkq123 <62462552+gokimkq123@users.noreply.github.com>
2020-09-24 13:32:40 +0900

    Update cubesolver.cpp

--- cubesolver.cpp
+++ cubesolver.cpp
@@ -6,9 +6,10 @@ int cube[6][3][3]; //색깔 : 흰색(0), 빨강(1), 파랑(2), 주황(3), 초록
 //큐브 공식 참고 사이트 : https://m.blog.naver.com/bluebirdkids/220691361547 
 void UO(){ //윗면 시계방향 (up original) 방향회전 참고 사이트 : https://cube3x3.com/%ED%81%90%EB%B8%8C%EB%A5%BC-%EB%A7%9E%EC%B6%94%EB%8A%94-%EB%B0%A9/ 
 	int temp[3][3], i, j;
-	temp[0][0] = cube[1][0][0];
-	temp[0][1] = cube[1][0][1];
-	temp[0][2] = cube[1][0][2];
+	
+	for(i=0 ; i < 3; i++){
+		temp[0][i] = cube[1][0][i];
+	}
 	for(i = 1; i <= 3; i++){
 		for(j = 0; j < 3; j++){
 			cube[i][0][j] = cube[i+1][0][j];
@@ -31,9 +32,10 @@ void UO(){ //윗면 시계방향 (up original) 방향회전 참고 사이트 : h
 
 void UU(){ //윗면 역시계방향 (up unverse)
 	int temp[3][3], i, j;
-	temp[0][0] = cube[4][0][0];
-	temp[0][1] = cube[4][0][1];
-	temp[0][2] = cube[4][0][2];
+	
+	for(i = 0 ; i < 3; i++){
+		temp[0][i] = cube[4][0][i];
+	}
 	for(i = 3; i >= 1; i--){
 		for(j = 0; j < 3; j++){
 			cube[i+1][0][j] = cube[i][0][j];
@@ -105,13 +107,35 @@ int main(){
 	}
 	UU();
 	for(i = 0; i < 6; i++){				 //test for functions work right
+		printf("\nside : %d\n", i);
 		for(j = 0; j < 3; j++){			 
 			for(k = 0; k < 3; k++){		 
 				printf("%d", cube[i][j][k]); 
 			}
 			printf("\n");
 		}
-		printf("\nside : %d\n", i);
 	}
 	//1단계 십자 맞추기 
 }
+
+/*
+input 
+0 0 0
+0 0 0
+0 0 0
+1 1 1
+1 1 1
+1 1 1
+2 2 2
+2 2 2
+2 2 2
+3 3 3
+3 3 3
+3 3 3
+4 4 4
+4 4 4
+4 4 4
+5 5 5
+5 5 5
+5 5 5
+*/
